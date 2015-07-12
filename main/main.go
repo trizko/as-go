792fd0ef050472cd71b837623d0301a8bec62c7e
Tony Rizko <tony@rizkocircle.com>
2015-07-12 15:57:32 -0700

    implement nodejs based audio-stripper app in Go

--- main/main.go
+++ main/main.go
@@ -5,11 +5,17 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func main() {
 	http.Handle("/", new(RequestHandler))
+	http.Handle("/download", new(DownloadHandler))
+
+	http.Handle("/img/", new(StaticHandler))
+	http.Handle("/css/", new(StaticHandler))
+	http.Handle("/js/", new(StaticHandler))
 
 	http.ListenAndServe(":8000", nil)
 }
@@ -23,6 +29,58 @@ type RequestHandler struct {
 func (handler *RequestHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%s - %s\n", req.Method, req.URL.Path)
 
+	path := "public/home.html"
+	file, err := os.Open(path)
+
+	if err == nil {
+		bufferedReader := bufio.NewReader(file)
+
+		w.Header().Set("Content-Type", "text/html")
+		bufferedReader.WriteTo(w)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+	}
+}
+
+// DownloadHandler will download a file from the user specified link
+type DownloadHandler struct {
+	http.Handler
+}
+
+func (handler *DownloadHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	stdout, err := exec.Command("youtube-dl", "--get-filename", "--restrict-filename", req.FormValue("url")).Output()
+	videoName := strings.TrimSpace(string(stdout))
+	musicName := videoName[:len(videoName)-1] + "3"
+
+	exec.Command("youtube-dl", "--restrict-filename", req.FormValue("url")).Run()
+
+	exec.Command("ffmpeg", "-i", videoName, "-vn", musicName).Run()
+
+	filename, err := os.Open(musicName)
+
+	if err == nil {
+		br := bufio.NewReader(filename)
+		w.Header().Set("Content-Disposition", "attachment; filename="+musicName)
+		w.Header().Set("Content-Type", "audio/mp3")
+
+		br.WriteTo(w)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte(err.Error()))
+	}
+
+	exec.Command("rm", "-rf", videoName, musicName).Run()
+}
+
+// StaticHandler will download a file from the user specified link
+type StaticHandler struct {
+	http.Handler
+}
+
+func (handler *StaticHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("%s - %s\n", req.Method, req.URL.Path)
+
 	path := "public/" + req.URL.Path
 	file, err := os.Open(path)
 
@@ -30,21 +88,20 @@ func (handler *RequestHandler) ServeHTTP(w http.ResponseWriter, req *http.Reques
 		bufferedReader := bufio.NewReader(file)
 		var contentType string
 
-		if strings.HasSuffix(path, ".css") {
+		if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else if strings.HasSuffix(path, ".css") {
 			contentType = "text/css"
-		} else if strings.HasSuffix(path, ".html") {
-			contentType = "text/html"
 		} else if strings.HasSuffix(path, ".js") {
 			contentType = "application/javascript"
-		} else if strings.HasSuffix(path, ".png") {
-			contentType = "image/png"
-		} else if strings.HasSuffix(path, ".mp4") {
-			contentType = "video/mp4"
 		} else {
 			contentType = "text/plain"
 		}
 
-		w.Header().Add("Content Type", contentType)
+		fmt.Println("contentType:")
+		fmt.Println(contentType)
+
+		w.Header().Set("Content-Type", contentType)
 		bufferedReader.WriteTo(w)
 	} else {
 		w.WriteHeader(404)
